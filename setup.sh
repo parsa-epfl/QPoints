4e45ac9e27f9e04ad97600e02c92592b0305ab0a
aansaarii <ali.ansari@epfl.ch>
2026-01-27 10:55:11 +0000

    support building gem5 in the qflex container

--- setup.sh
+++ setup.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
+if command -v apt-get >/dev/null 2>&1; then
+  if command -v sudo >/dev/null 2>&1; then
+    SUDO="sudo"
+  else
+    SUDO=""
+  fi
+  $SUDO apt-get update
+  $SUDO apt-get remove -y scons
+  $SUDO apt-get install -y \
+    build-essential \
+    bzip2 \
+    git \
+    libboost-all-dev \
+    libprotobuf-dev \
+    libsqlite3-dev \
+    m4 \
+    pkg-config \
+    protobuf-compiler \
+    python3 \
+    python3-dev \
+    python3-pip \
+    python3-six \
+    wget \
+    zlib1g-dev
+  python3 -m pip install "scons==3.1.2"
+fi
+
 echo Creating required directories
 mkdir checkpoints
 
 echo cloning gem5 repo
 git clone -b cassandra-issue-fix https://github.com/bgodala/gem5_ARM_FDIP.git  gem5
+cp scripts/qflex/gem5_patch/init_signals.cc gem5/src/sim/init_signals.cc 
+
+echo build gem5
+cd gem5
+scons -j8 build/ARM/gem5.opt CXX=g++ CXXFLAGS="-std=c++17"
+cd ..
 
 echo Pulling Docker image
 docker pull cloudsuitetest/gem5-qpoints
